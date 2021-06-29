f4903b6bf0d2ea85048f4d8ff864735488993a9c
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-06-28 19:10:22 -0500

    alter service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -8,18 +8,32 @@ export class TodoService {
   todoList: ITodo [] = [
     // example of how to make an item in todo list
     { title: 'Install Angular CLI', id: this.todoId, status: 'Todo', createdAt: new Date()},
-  
-  ]
+  ];
+
+  statuses: string[] = ['Todo', 'Doing', 'Done'];
+
   constructor() { }
-  getTodos(){
-    return this.todoList;
+
+  getTodos(status?: string): ITodo[] {
+
+    if(!status){
+      return this.todoList;
+    }
+
+    return this.todoList.filter(todo => todo.status === status);
   }
+
   deleteTodo(todo: ITodo) {
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList.splice(index, 1);
   }
+
   addTodo(todo: ITodo):void {
     todo.id = ++this.todoId;
     this.todoList.push(todo);
+  }
+
+  getStatuses(): string[] {
+    return this.statuses;
   }
 }
