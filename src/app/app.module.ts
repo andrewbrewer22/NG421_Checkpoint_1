9fccbd1d5a223993f44b481ab696ea59e2aeebc3
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-06-28 20:43:37 -0500

    routes working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,18 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { CreateTodoComponent } from './create-todo/create-todo.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
-import {RouterModule } from '@angular/router';
+import {RouterModule, Route } from '@angular/router';
 import { BoardComponent } from './board/board.component';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
 import { TodoTableComponent } from './todo-table/todo-table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
 
-const routes = [];
+const routes: Route[] = [
+  {path: 'table', component: TodoTableComponent},
+  {path: 'board', component: BoardComponent},
+  {path: '**', component: BoardComponent}
+];
 const appRoutes = RouterModule.forRoot(routes);
 
 @NgModule({
